a2aa0aa26a8b35f6e5b2c43df7244a7c4561de2d
danstarns <danielstarns@hotmail.com>
2023-11-11 20:24:43 +0700

    fix: links footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -34,19 +34,20 @@ const urlGroups = [
 
 export function Footer() {
   return (
-    <div className="text-white bg-graphql-blue py-10 gap-5 mt-auto flex flex-col align-center justify-center">
-      <div className="flex gap-10 mx-auto">
-        {urlGroups.map((urls, index) => (
-          <UrlList key={index} urls={urls} />
-        ))}
-      </div>
-
-      <div className="pt-5">
-        <p className="text-center">
-          <a href="https://rocketconnect.co.uk">
-            Made with <span className="text-red-500">❤️</span>
-          </a>
-        </p>
+    <div className="text-white bg-graphql-blue py-10 mt-auto">
+      <div className="flex flex-col items-center justify-center gap-5 sm:gap-10">
+        <div className="w-full flex flex-col sm:flex-row sm:justify-center gap-5">
+          {urlGroups.map((urls, index) => (
+            <UrlList key={index} urls={urls} />
+          ))}
+        </div>
+        <div className="w-full pt-5">
+          <p className="text-center">
+            <a href="https://rocketconnect.co.uk">
+              Made with <span className="text-red-500">❤️</span>
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
