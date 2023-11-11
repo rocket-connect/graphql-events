e721af7ef6defe086b82e34e66056a4d8fe796a0
danstarns <danielstarns@hotmail.com>
2023-11-11 20:14:09 +0700

    refactor: *

--- client/src/components/events.tsx
+++ client/src/components/events.tsx
@@ -19,7 +19,7 @@ export function Events() {
   return (
     <div className="drop-shadow-lg w-full bg-white grow border-t-2 border-graphql-border p-4">
       <div className="container mx-auto py-4">
-        <h2 className="text-2xl mb-4">Events</h2>
+        <h2 className="text-3xl font-bold mb-4">Events</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {events.map((event) => (
             <div
--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,69 +1,50 @@
-export function Footer() {
-  const urlsGroupOne = [
+const UrlList = ({ urls }) => (
+  <ul className="marker:text-graphql-pink list-disc ml-6">
+    {urls.map((url) => (
+      <li key={url}>
+        <a
+          href={url}
+          className="block p-1 hover:text-graphql-pink transition-colors duration-300"
+        >
+          {url}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+const urlGroups = [
+  [
     "https://graphql.org/",
     "https://graphql.org/foundation/",
     "https://twitter.com/GraphQL",
-  ];
-
-  const urlsGroupTwo = [
+  ],
+  [
     "https://www.meetup.com/graphql-sf/",
     "https://www.meetup.com/graphql-bangkok/",
     "https://www.meetup.com/graphql-sg/",
     "https://guild.host/graphql-london/",
     "https://www.meetup.com/graphql-seattle",
-  ];
-
-  const urlsGroupThree = [
+  ],
+  [
     "https://github.com/rocket-connect/graphql-events",
     "https://www.graphql-events.org/",
-  ];
+  ],
+];
 
+export function Footer() {
   return (
     <div className="text-white bg-graphql-blue py-10 gap-5 mt-auto flex flex-col align-center justify-center">
-      <div className="flex gap-10 mx-auto ">
-        <ul className="marker:text-graphql-pink list-disc ml-6">
-          {urlsGroupOne.map((url) => (
-            <li key={url}>
-              <a
-                href={url}
-                className="block p-1 hover:text-graphql-pink transition-colors duration-300"
-              >
-                {url}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <ul className="marker:text-graphql-pink list-disc ml-6">
-          {urlsGroupTwo.map((url) => (
-            <li key={url}>
-              <a
-                href={url}
-                className="block p-1 hover:text-graphql-pink transition-colors duration-300"
-              >
-                {url}
-              </a>
-            </li>
-          ))}
-        </ul>
-
-        <ul className="marker:text-graphql-pink list-disc ml-6">
-          {urlsGroupThree.map((url) => (
-            <li key={url}>
-              <a
-                href={url}
-                className="block p-1 hover:text-graphql-pink transition-colors duration-300"
-              >
-                {url}
-              </a>
-            </li>
-          ))}
-        </ul>
+      <div className="flex gap-10 mx-auto">
+        {urlGroups.map((urls, index) => (
+          <UrlList key={index} urls={urls} />
+        ))}
       </div>
 
       <div className="pt-5">
         <p className="text-center">
           <a href="https://rocketconnect.co.uk">
-            Made with <span className="text-red-500">❤️ </span>
+            Made with <span className="text-red-500">❤️</span>
           </a>
         </p>
       </div>
