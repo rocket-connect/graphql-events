9cff409c536aec3a7a4ae27d562a9b574dd15ae2
Harry Sisley <harry.sisley@outlook.com>
2024-01-04 16:18:52 +0000

    move meetups to api

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import path from "path";
 import { events } from "./events";
+import { meetups } from "./meetups";
 
 export const app: Express = express();
 
@@ -10,6 +11,11 @@ app.use(express.json());
 app.get("/api/events", (req, res) => {
   res.json(events);
 });
+
+app.get("/api/meetups", (req, res) => {
+  res.json(meetups);
+});
+
 app.use(express.static(path.join(__dirname, "../../client/build")));
 app.use(express.static("public"));
 app.get("*", (req, res) => {
